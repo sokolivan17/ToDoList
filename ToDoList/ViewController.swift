b04fd0b1759067ad69b022b9db60009a79a51081
Ivan Sokol <sokolivan34@mail.ru>
2023-09-01 12:03:55 +0300

    Deleted main file, created scene.

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "To Do List"
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
 
